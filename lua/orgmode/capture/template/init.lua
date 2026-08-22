0a91df7c4f0fe64c84cf801ef0f88acc149849b3
Fredrik Holmberg <fredrik.holmber@3ug.se>
2026-08-22 23:17:19 +0200

    finally resolved mergeconflict from master

--- lua/orgmode/capture/template/init.lua
+++ lua/orgmode/capture/template/init.lua
@@ -83,7 +83,6 @@ local expansions = {
 ---@field template? string|string[]
 ---@field target? string
 ---@field datetree? OrgCaptureTemplateDatetree
----@field headline? string|fun():string
 ---@field prepend? boolean
 ---@field headline? string|fun(OrgFile?):string
 ---@field regexp? string
